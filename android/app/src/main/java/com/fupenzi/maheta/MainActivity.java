3c48afc769134627d2df8b2acef28c58820773d5
Piotr Nowak <npiotrek44@gmail.com>
2022-07-10 17:11:12 +0200

    Add capactior, prettier, linters

---
+++ android/app/src/main/java/com/fupenzi/maheta/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fupenzi.maheta;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fupenzi.maheta',
+  appName: 'Maheta',
+  webDir: 'dist/maheta',
+  bundledWebRuntime: false
+};
+
+export default config;
