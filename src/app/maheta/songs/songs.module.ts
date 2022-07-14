407153005ab41dcee08795e8118e753fd377d053
Piotr Nowak <npiotrek44@gmail.com>
2022-07-14 21:45:15 +0200

    Add song component

---
+++ src/app/maheta/songs/songs.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SongsComponent } from './songs.component';
+
+@NgModule({
+  declarations: [SongsComponent],
+  imports: [CommonModule],
+})
+export class SongsModule {}
