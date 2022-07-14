4c24b92a7bca54b5d09fe597f9a11993433c7b34
Piotr Nowak <npiotrek44@gmail.com>
2022-07-14 18:10:47 +0200

    Update bottom navigation

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -2,12 +2,36 @@ import { Component } from '@angular/core';
 
 import { ThemeClassEnum, ThemeService } from '@app/modules/layout/services/theme.service';
 
+interface BottomNavTab {
+  icon: string;
+  name: string;
+}
+
 @Component({
   selector: 'maheta-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
+  public bottomNavTabs: BottomNavTab[] = [
+    {
+      icon: 'play_arrow',
+      name: 'Player',
+    },
+    {
+      icon: 'library_music',
+      name: 'Songs',
+    },
+    {
+      icon: 'album',
+      name: 'Albums',
+    },
+    {
+      icon: 'playlist_play',
+      name: 'Playlists',
+    },
+  ];
+
   constructor(private themeService: ThemeService) {}
 
   public switchTheme(): void {
