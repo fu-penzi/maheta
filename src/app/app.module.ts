5cabb7dc0960ad5c7642a3b1f9e0a98111de7e45
Piotr Nowak <npiotrek44@gmail.com>
2022-07-14 22:19:35 +0200

    Change default link styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './modules/layout/layout.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SongsComponent } from './maheta/songs/songs.component';
 
 @NgModule({
   declarations: [AppComponent],
