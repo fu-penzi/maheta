a63098649b2c30e28240db3994ebd4823b58e2cc
Piotr Nowak <npiotrek44@gmail.com>
2022-07-18 15:56:48 +0200

    Add live reload on Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
+// insert your ip here
+const hostIp = '192.168.0.105';
+
 const config: CapacitorConfig = {
   appId: 'com.fupenzi.maheta',
   appName: 'Maheta',
   webDir: 'dist/maheta',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: `http://${hostIp}:8100`,
+    cleartext: true,
+  },
 };
 
 export default config;
