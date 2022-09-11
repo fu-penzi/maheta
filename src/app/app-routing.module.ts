ca54336f183be356d15946085e8ebf33aa3f857d
Piotr Nowak <npiotrek44@gmail.com>
2022-09-11 13:12:12 +0200

    Add preloading lazy modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { LayoutChildrenGuard } from '@src/app/guards/layout-children.guard';
 import { UrlEnum } from '@src/app/model/url.enum';
@@ -35,7 +35,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
