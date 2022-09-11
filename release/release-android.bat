e824c431098b75d986f819269f89c91f47cac582
Piotr Nowak <npiotrek44@gmail.com>
2022-09-11 19:39:39 +0200

    Generate release-apk v0.0.1

--- release/release-android.bat
+++ release/release-android.bat
@@ -6,7 +6,9 @@ set BUILD_TOOLS="C:\Users\Fupenzi\AppData\Local\Android\Sdk\build-tools\32.0.0"
 set KEYSTORE="keystore.jks"
 
 call cd ..\android
+call del /Q "..\release\android\*"
+call del /Q "%APK_DIRNAME%\*"
 call ./gradlew assembleRelease
 call jarsigner -keystore "..\%KEYSTORE%" -storepass %STOREPASS% %APK_DIRNAME%/app-release-unsigned.apk %KEY_ALIAS%
 call %BUILD_TOOLS%\zipalign.exe  4 %APK_DIRNAME%/app-release-unsigned.apk %APK_DIRNAME%/app-release.apk
-call copy "%APK_DIRNAME%\app-release.apk" "..\release\android"
+call copy "%APK_DIRNAME%\app-release.apk" "..\release\android\app-release_v%3.apk"
