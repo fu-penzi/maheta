63da55d5571bec1603a4d7be7e82678dab9b8492
Piotr Nowak <npiotrek44@gmail.com>
2023-01-13 13:23:13 +0100

    Add base component, fix slow albums loading

--- src/app/modules/shared/base.component.ts
+++ src/app/modules/shared/base.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnDestroy } from '@angular/core';
 
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Component({ template: '' })
 export abstract class BaseComponent implements OnDestroy {
-  public onDestroy$: Subject<void> = new Subject<void>();
+  private _onDestroy$: Subject<void> = new Subject<void>();
+
+  public get onDestroy$(): Observable<void> {
+    return this._onDestroy$.asObservable();
+  }
 
   public ngOnDestroy(): void {
-    this.onDestroy$.next();
+    this._onDestroy$.next();
+    this._onDestroy$.complete();
   }
 }
