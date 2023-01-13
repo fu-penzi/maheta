8477f0783560777f4726d8bef668a686778d52b6
Piotr Nowak <npiotrek44@gmail.com>
2023-01-13 19:40:48 +0100

    Add internationalization

--- src/app/db/collections/collection.model.ts
+++ src/app/db/collections/collection.model.ts
@@ -1,7 +1,5 @@
-import { Playlist, PlaylistPopulationEnum } from '@src/app/db/domain/playlist.schema';
-import { Track } from '@src/app/db/domain/track.schema';
+import { PlaylistPopulationEnum } from '@src/app/db/domain/playlist.schema';
 
 type DocumentIndex = 'uri' | 'id';
 export type DocumentSelector = Partial<Record<DocumentIndex, string>>;
-export type CollectionDocument = Track | Playlist;
 export type CollectionPopulationEnum = PlaylistPopulationEnum;
