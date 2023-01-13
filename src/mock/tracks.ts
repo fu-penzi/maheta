010901dfc3c789a76ac83a4e1f4131c592f1f832
Piotr Nowak <npiotrek44@gmail.com>
2023-01-13 13:23:13 +0100

    Add base component, fix slow albums loading

--- src/mock/tracks.ts
+++ src/mock/tracks.ts
@@ -2,8 +2,12 @@ import { Capacitor } from '@capacitor/core';
 
 import { getTrackObject, Track } from '@src/app/db/domain/track.schema';
 const tracks: Track[] = [];
-getTrackObject('assets/vp.mp3', Capacitor.convertFileSrc('assets/vp.mp3')).then((track) =>
-  tracks.push(track)
-);
+Array(500)
+  .fill(1)
+  .map((a, i) =>
+    getTrackObject(`vp${i + 1}.mp3`, Capacitor.convertFileSrc('assets/vp.mp3')).then((track) =>
+      tracks.push(track)
+    )
+  );
 
 export const tracksMock: Track[] = tracks;
