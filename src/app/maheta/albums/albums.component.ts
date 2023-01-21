a10483904fc5682f90b0c7493da170d0ebd93430
Piotr Nowak <npiotrek44@gmail.com>
2023-01-21 02:21:01 +0100

    Add Albums progressive loading

--- src/app/maheta/albums/albums.component.ts
+++ src/app/maheta/albums/albums.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { Album } from '@src/app/db/domain/album';
 import { BaseComponent } from '@src/app/modules/shared/base.component';
@@ -12,7 +12,11 @@ import { takeUntil } from 'rxjs';
   styleUrls: ['./albums.component.scss'],
 })
 export class AlbumsComponent extends BaseComponent implements OnInit {
-  public albums: Album[] = [];
+  @ViewChild('itemsContainer', { read: ViewContainerRef }) container: ViewContainerRef;
+  @ViewChild('item', { read: TemplateRef }) template: TemplateRef<any>;
+
+  private _albums: Album[] = [];
+
   constructor(private musicLibraryAlbumsService: MusicLibraryAlbumsService) {
     super();
   }
@@ -20,6 +24,33 @@ export class AlbumsComponent extends BaseComponent implements OnInit {
   public ngOnInit(): void {
     this.musicLibraryAlbumsService.albums$
       .pipe(takeUntil(this.onDestroy$))
-      .subscribe((albums: Album[]) => (this.albums = albums));
+      .subscribe((albums: Album[]) => {
+        this._albums = albums;
+        this.buildData(albums.length);
+      });
+  }
+
+  private buildData(length: number): void {
+    const ITEMS_RENDERED_AT_ONCE = 10;
+    const INTERVAL_IN_MS = 10;
+
+    let currentIndex = 0;
+
+    const interval = setInterval(() => {
+      const nextIndex = currentIndex + ITEMS_RENDERED_AT_ONCE;
+
+      for (let i = currentIndex; i <= nextIndex; i++) {
+        if (i >= length) {
+          clearInterval(interval);
+          break;
+        }
+        const context = {
+          $implicit: this._albums[i],
+        };
+        this.container.createEmbeddedView(this.template, context);
+      }
+
+      currentIndex += ITEMS_RENDERED_AT_ONCE;
+    }, INTERVAL_IN_MS);
   }
 }
