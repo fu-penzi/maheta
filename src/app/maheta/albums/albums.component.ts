ddb8a93164f009cf3b7142d579ecc82e89cb7451
Piotr Nowak <npiotrek44@gmail.com>
2023-01-21 11:25:29 +0100

    Fix duplicated albums

--- src/app/maheta/albums/albums.component.ts
+++ src/app/maheta/albums/albums.component.ts
@@ -4,7 +4,7 @@ import { Album } from '@src/app/db/domain/album';
 import { BaseComponent } from '@src/app/modules/shared/base.component';
 import { MusicLibraryAlbumsService } from '@src/app/services/music-library/music-library-albums.service';
 
-import { takeUntil } from 'rxjs';
+import { interval, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'maheta-albums',
@@ -15,7 +15,11 @@ export class AlbumsComponent extends BaseComponent implements OnInit {
   @ViewChild('itemsContainer', { read: ViewContainerRef }) container: ViewContainerRef;
   @ViewChild('item', { read: TemplateRef }) template: TemplateRef<any>;
 
+  public firstChunk: Album[] = [];
+
   private _albums: Album[] = [];
+
+  private readonly _firstChunkSize: number = 9;
 
   constructor(private musicLibraryAlbumsService: MusicLibraryAlbumsService) {
     super();
@@ -26,22 +30,18 @@ export class AlbumsComponent extends BaseComponent implements OnInit {
       .pipe(takeUntil(this.onDestroy$))
       .subscribe((albums: Album[]) => {
         this._albums = albums;
-        this.buildData(albums.length);
+        this.firstChunk = albums.slice(0, this._firstChunkSize);
+        this.buildData(this._firstChunkSize, albums.length);
       });
   }
 
-  private buildData(length: number): void {
-    const ITEMS_RENDERED_AT_ONCE = 10;
-    const INTERVAL_IN_MS = 10;
+  private buildData(startIndex: number, length: number): void {
+    const ITEMS_RENDERED_AT_ONCE = 3;
 
-    let currentIndex = 0;
-
-    const interval = setInterval(() => {
-      const nextIndex = currentIndex + ITEMS_RENDERED_AT_ONCE;
-
-      for (let i = currentIndex; i <= nextIndex; i++) {
+    let currentIndex = startIndex;
+    interval(10).subscribe(() => {
+      for (let i = currentIndex; i < currentIndex + ITEMS_RENDERED_AT_ONCE; i++) {
         if (i >= length) {
-          clearInterval(interval);
           break;
         }
         const context = {
@@ -49,8 +49,7 @@ export class AlbumsComponent extends BaseComponent implements OnInit {
         };
         this.container.createEmbeddedView(this.template, context);
       }
-
       currentIndex += ITEMS_RENDERED_AT_ONCE;
-    }, INTERVAL_IN_MS);
+    });
   }
 }
