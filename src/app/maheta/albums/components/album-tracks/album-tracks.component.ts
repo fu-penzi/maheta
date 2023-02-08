7fcb9ea77b63b4f48b3406735cd818a55225061b
Piotr Nowak <npiotrek44@gmail.com>
2023-02-08 17:27:42 +0100

    feat: Album tracks header

--- src/app/maheta/albums/components/album-tracks/album-tracks.component.ts
+++ src/app/maheta/albums/components/album-tracks/album-tracks.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
 
 import { Album } from '@src/app/db/domain/album';
 import { Track } from '@src/app/db/domain/track.schema';
+
+import { sum } from 'lodash';
 
 @Component({
   selector: 'maheta-album-tracks',
@@ -9,9 +11,14 @@ import { Track } from '@src/app/db/domain/track.schema';
   styleUrls: ['./album-tracks.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlbumTracksComponent {
+export class AlbumTracksComponent implements OnChanges {
   @Input() public album: Album;
   @Input() public currentTrack: Track | null;
 
+  public totalAlbumTime: number = 0;
   constructor() {}
+
+  public ngOnChanges(): void {
+    this.totalAlbumTime = sum(this.album.tracks.map((track: Track) => track.duration));
+  }
 }
--- src/app/pipes/duration.pipe.ts
+++ src/app/pipes/duration.pipe.ts
@@ -1,16 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+export interface DurationPipeConfig {
+  noFallback?: boolean;
+  hourMinuteFormat?: boolean;
+}
+
 @Pipe({
   name: 'mahetaDuration',
 })
 export class DurationPipe implements PipeTransform {
-  transform(totalSeconds: number | undefined | null, noFallback?: boolean): string {
+  transform(totalSeconds: number | undefined | null, config?: DurationPipeConfig): string {
     if (!totalSeconds) {
-      return noFallback ? '' : '0:00';
+      return config?.noFallback ? '' : '0:00';
     }
 
-    const seconds: number = totalSeconds % 60;
-    const minutes: number = totalSeconds / 60;
+    const seconds: number = Math.floor(totalSeconds % 60);
+    const minutes: number = Math.floor(totalSeconds / 60);
+    const hours: number = Math.floor(totalSeconds / 3600);
+
+    if (config?.hourMinuteFormat) {
+      let duration: string = '';
+      if (hours) {
+        duration += hours + 'h ';
+      }
+      if (minutes - hours * 60) {
+        duration += minutes - hours * 60 + ' min';
+      }
+      return duration;
+    }
+
     return Math.floor(minutes) + ':' + `${Math.floor(seconds)}`.padStart(2, '0');
   }
 }
